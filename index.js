d383370f17ad043dc1b1743c3f6f5b658511bb1a
AndreyBegma <abegma137@gmail.com>
2023-02-24 13:59:21 +0200

    Add empty server

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+
+const config = require('./config')
+
+const mongoose = require('mongoose')
+
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const http = require('http')
+const server = http.createServer(app)
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(cors(config.cors))
+
+server.listen(config.port, () => { 
+    console.log(`Server listening on port ${config.port}`)
+})
